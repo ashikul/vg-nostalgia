6ebcb9064b717cf4aadc1db83191cdb15e0ad598
Ashikul <alam.ashikul@gmail.com>
2016-03-10 18:46:13 -0500

    added constant

---
+++ client/app/app.constant.js
@@ -0,0 +1,9 @@
+(function (angular, undefined) {
+    'use strict';
+
+    angular.module('vgNostalgiaApp.constants', [])
+
+        .constant('appConfig', {userRoles: ['guest', 'user', 'admin']})
+
+    ;
+})(angular);
